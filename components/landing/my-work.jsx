7da6b90f4f5e795dc1f5cf8e934e9962c8726626
Gonzalo Salmeron <gsalmeron@enerclic.es>
2023-09-22 10:30:55 +0200

    style: Add description on section "my work"

--- components/landing/my-work.jsx
+++ components/landing/my-work.jsx
@@ -7,10 +7,16 @@ function MyWork() {
   const projects = getProjects();
 
   return (
-    <section id="my-work" className="w-full grid grid-cols-3 bg-projects-pattern">
-      {projects.map((project, i) => (
-        <ProjectBox project={JSON.stringify(project)} key={i} />
-      ))}
+    <section id="my-work" className="w-full py-10">
+      <div className="w-full py-4 px-responsive mb-2">
+        {/* <p className="px-3 py-0.5 text-sm bg-purple-200 text-purple-600 inline-flex rounded-full font-medium mb-1.5">Trayectory</p> */}
+        <h2 className="text-white text-4xl font-semibold uppercase">My Work</h2>
+      </div>
+      <div className="grid grid-cols-3 bg-projects-pattern">
+        {projects.map((project, i) => (
+          <ProjectBox project={JSON.stringify(project)} key={i} />
+        ))}
+      </div>
     </section>
   );
 }
